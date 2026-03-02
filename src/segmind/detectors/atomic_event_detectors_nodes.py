fe7dcab3b92a622172ed350fd058cd5dcac6ef77
sorin <sorin@uni-bremen.de>
2026-03-02 14:10:29 +0100

    added new class atomic_event_detector_nodes, which will replace the old atomic event detectors class, working currently for contact and lossOfContact, updated the test_statechart.py

---
+++ src/segmind/detectors/atomic_event_detectors_nodes.py
@@ -0,0 +1,187 @@
+from dataclasses import dataclass, field
+from typing import Optional, List, Dict, Set, Any
+
+from giskardpy.motion_statechart.context import SegmindContext, MotionStatechartContext
+from giskardpy.motion_statechart.data_types import ObservationStateValues
+from giskardpy.motion_statechart.graph_node import MotionStatechartNode
+from giskardpy.motion_statechart.motion_statechart import MotionStatechart
+from segmind.datastructures.events import Event, ContactEvent, LossOfContactEvent
+from semantic_digital_twin.reasoning.predicates import contact
+from semantic_digital_twin.world_description.connections import Connection6DoF
+from semantic_digital_twin.world_description.world_entity import Body
+
+
+@dataclass
+class SegmindContext(MotionStatechartContext):
+    latest_contact_bodies: Dict[Body, Set[Body]] = None
+    latest_support: Optional[Dict[Body, Body]] = None
+    # ToDo:  Why circular import for EventLogger?
+    logger: Optional[Any] = None
+
+
+class DetectorStateChart(MotionStatechart):
+    pass
+
+
+@dataclass(eq=False, repr=False)
+class ContactDetector(MotionStatechartNode):
+    tracked_object: Optional[Body] = field(kw_only=True, default=None)
+    context: SegmindContext = field(kw_only=True)
+
+    def on_tick(
+        self, context: MotionStatechartContext
+    ) -> Optional[ObservationStateValues]:
+        trackable_objects = [
+            body
+            for body in self.context.world.bodies
+            if type(body.parent_connection) is Connection6DoF
+        ]
+        objects_to_check = (
+            [self.tracked_object] if self.tracked_object else trackable_objects
+        )
+
+        events = self.update_latest_contact_bodies_and_trigger_events(objects_to_check)
+        for e in events:
+            self.context.logger.log_event(e)
+
+        if events:
+            for e in events:
+                self.context.logger.log_event(e)
+
+            return ObservationStateValues.TRUE
+
+        return ObservationStateValues.FALSE
+
+    def print_something(self):
+        trackable_objects = [
+            body
+            for body in self.context.world.bodies
+            if type(body.parent_connection) is Connection6DoF
+        ]
+        print(trackable_objects)
+
+    def get_contact_bodies(self, tracked_objects: List[Body]) -> Dict[Body, Set[Body]]:
+        contact_bodies = {}
+        for obj in tracked_objects:
+            for body in self.context.world.bodies_with_collision:
+                if body is obj:
+                    continue
+                if contact(obj, body):
+                    contact_bodies[obj] = contact_bodies.get(obj, set()) | {body}
+        return contact_bodies
+
+    def update_latest_contact_bodies_and_trigger_events(
+        self,
+        tracked_objects: List[Body],
+    ) -> List[Event]:
+        new_contact_pairs = self.get_contact_bodies(tracked_objects)
+        latest_contact_bodies = self.context.latest_contact_bodies
+        events = []
+        for obj, contact_list in new_contact_pairs.items():
+            if obj not in latest_contact_bodies:
+                latest_contact_bodies[obj] = contact_list
+                for body in contact_list:
+                    events.append(
+                        ContactEvent(
+                            close_bodies=contact_list,
+                            latest_close_bodies=self.context.latest_contact_bodies,
+                            of_object=obj,
+                            with_object=body,
+                        )
+                    )
+
+            elif obj in latest_contact_bodies:
+                latest_contact_bodies[obj] |= contact_list
+                for body in contact_list:
+                    events.append(
+                        ContactEvent(
+                            close_bodies=contact_list,
+                            latest_close_bodies=self.context.latest_contact_bodies,
+                            of_object=obj,
+                            with_object=body,
+                        )
+                    )
+        self.context.latest_contact_bodies = latest_contact_bodies
+        return events
+
+
+@dataclass(eq=False, repr=False)
+class LossOfContactDetector(MotionStatechartNode):
+    tracked_object: Optional[Body] = field(kw_only=True, default=None)
+    context: SegmindContext = field(kw_only=True)
+
+    def on_tick(
+        self, context: MotionStatechartContext
+    ) -> Optional[ObservationStateValues]:
+        trackable_objects = [
+            body
+            for body in self.context.world.bodies
+            if type(body.parent_connection) is Connection6DoF
+        ]
+        objects_to_check = (
+            [self.tracked_object] if self.tracked_object else trackable_objects
+        )
+        # self.context.latest_contact_bodies = self.update_latest_contact_bodies(objects_to_check)
+        # Now we need to trigger the event
+        events = self.update_latest_contact_bodies_and_trigger_events(objects_to_check)
+        for e in events:
+            self.context.logger.log_event(e)
+        if events:
+            for e in events:
+                self.context.logger.log_event(e)
+            return ObservationStateValues.TRUE
+        return ObservationStateValues.FALSE
+
+    def print_something(self):
+        trackable_objects = [
+            body
+            for body in self.context.world.bodies
+            if type(body.parent_connection) is Connection6DoF
+        ]
+        print(trackable_objects)
+
+    def get_contact_bodies(self, tracked_objects: List[Body]) -> Dict[Body, Set[Body]]:
+        contact_bodies = {}
+        for obj in tracked_objects:
+            for body in self.context.world.bodies_with_collision:
+                if body is obj:
+                    continue
+                if contact(obj, body):
+                    contact_bodies[obj] = contact_bodies.get(obj, set()) | {body}
+        return contact_bodies
+
+    def update_latest_contact_bodies_and_trigger_events(
+        self,
+        tracked_objects: List[Body],
+    ) -> List[Event]:
+        new_contact_pairs = self.get_contact_bodies(tracked_objects)
+        latest_contact_bodies = self.context.latest_contact_bodies
+        events = []
+        for obj, contact_list in list(latest_contact_bodies.items()):
+            if obj not in new_contact_pairs:
+                latest_contact_bodies.pop(obj)
+                for body in contact_list:
+                    events.append(
+                        LossOfContactEvent(
+                            close_bodies=contact_list,
+                            latest_close_bodies=self.context.latest_contact_bodies,
+                            of_object=obj,
+                            with_object=body,
+                        )
+                    )
+
+            elif obj in new_contact_pairs:
+                new_contacts = new_contact_pairs[obj]
+                lost_contact = contact_list - new_contacts
+                for body in lost_contact:
+                    events.append(
+                        LossOfContactEvent(
+                            close_bodies=contact_list,
+                            latest_close_bodies=self.context.latest_contact_bodies,
+                            of_object=obj,
+                            with_object=body,
+                        )
+                    )
+                latest_contact_bodies[obj] = new_contacts
+        self.context.latest_contact_bodies = latest_contact_bodies
+        return events
--- test/test_statechart.py
+++ test/test_statechart.py
@@ -1,28 +1,33 @@
 import time
+from collections import defaultdict
+from typing import Dict, DefaultDict, Set
 
 import rclpy
 
+from Segmind.test import setup_contact_world, setup_support_world
 from krrood.symbolic_math.symbolic_math import trinary_logic_or
-from segmind.datastructures.events import ContactEvent, LossOfContactEvent, SupportEvent, LossOfSupportEvent
-from segmind.detectors.atomic_event_detectors import (
+from segmind.datastructures.events import (
+    ContactEvent,
+    LossOfContactEvent,
+    SupportEvent,
+    LossOfSupportEvent,
+)
+from segmind.detectors.atomic_event_detectors_nodes import (
     DetectorStateChart,
     ContactDetector,
-    ContactDetectorNode, LossOfContactDetectorNode,
+    LossOfContactDetector,
 )
 from segmind.detectors.spatial_relation_detector import SupportDetectorNode
 from segmind.event_logger import EventLogger
 
 from giskardpy.executor import Executor
-from giskardpy.motion_statechart.context import MotionStatechartContext
+from giskardpy.motion_statechart.context import SegmindContext
 from giskardpy.motion_statechart.goals.templates import Sequence
-from giskardpy.motion_statechart.graph_node import MotionStatechartNode
-from giskardpy.motion_statechart.motion_statechart import MotionStatechart
-from giskardpy.motion_statechart.test_nodes.test_nodes import ChangeStateOnEvents
 from semantic_digital_twin.adapters.ros.visualization.viz_marker import (
     VizMarkerPublisher,
 )
 from semantic_digital_twin.spatial_types import HomogeneousTransformationMatrix
-from test import setup_contact_world, setup_support_world
+from semantic_digital_twin.world_description.world_entity import Body
 
 
 class TestMotionStatechart:
@@ -33,27 +38,42 @@ class TestMotionStatechart:
         sc = DetectorStateChart()
         logger = EventLogger()
         cylinder = world.get_body_by_name("cylinder_body")
-        contact_detector = ContactDetectorNode(
-            name="contact_detector", logger=logger, tracked_object=cylinder
+        self.context = SegmindContext(
+            world=world,
+            logger=logger,
+            latest_contact_bodies={},
+        )
+        kin_sim = Executor(self.context)
+
+        contact_detector = ContactDetector(
+            name="contact_detector", context=self.context, tracked_object=cylinder
+        )
+        loss_of_contact_detector = LossOfContactDetector(
+            name="loss_of_contact_detector",
+            context=self.context,
+            tracked_object=cylinder,
         )
 
-        loss_of_contact_detector = LossOfContactDetectorNode(
-            name="loss_of_contact_detector", logger=logger, tracked_object=cylinder)
-
-        sc.add_nodes([Sequence([contact_detector]),
-                      Sequence([loss_of_contact_detector])
-                      ])
-
-        kin_sim = Executor(MotionStatechartContext(world=world))
+        sc.add_nodes([contact_detector, loss_of_contact_detector])
 
         kin_sim.compile(motion_statechart=sc)
         kin_sim.tick()
         # No Contact yet
-        assert len([i for i in logger.get_events() if isinstance(i, ContactEvent)]) == 0
+        assert (
+            len(
+                [
+                    i
+                    for i in self.context.logger.get_events()
+                    if isinstance(i, ContactEvent)
+                ]
+            )
+            == 0
+        )
         assert contact_detector.observation_state == 0.0
+        assert loss_of_contact_detector.observation_state == 0.0
 
-        cylinder.parent_connection.origin = HomogeneousTransformationMatrix.from_xyz_rpy(
-            y=-0.4
+        cylinder.parent_connection.origin = (
+            HomogeneousTransformationMatrix.from_xyz_rpy(y=-0.4)
         )
         kin_sim.tick()
 
@@ -61,17 +81,21 @@ class TestMotionStatechart:
         assert len([i for i in logger.get_events() if isinstance(i, ContactEvent)]) == 2
         assert contact_detector.observation_state == 1.0
 
-        cylinder.parent_connection.origin = HomogeneousTransformationMatrix.from_xyz_rpy(
-            z=2
+        cylinder.parent_connection.origin = (
+            HomogeneousTransformationMatrix.from_xyz_rpy(z=2)
         )
 
         kin_sim.tick()
 
-        kin_sim.motion_statechart.draw("/home/sorin/dev/Segmind/test/img/" + "sony.pdf")
-        assert len([i for i in logger.get_events() if isinstance(i, LossOfContactEvent)]) == 2
+        kin_sim.motion_statechart.draw(
+            "/home/sorin/dev/workspace/Segmind/test/img/" + "sony.pdf"
+        )
+        assert (
+            len([i for i in logger.get_events() if isinstance(i, LossOfContactEvent)])
+            == 2
+        )
         assert loss_of_contact_detector.observation_state == 1.0
         rclpy.shutdown()
-
 
     def test_support_detector(self):
         world = setup_support_world()
@@ -82,31 +106,62 @@ class TestMotionStatechart:
         cylinder = world.get_body_by_name("cylinder_body")
         table = world.get_body_by_name("table_body")
         cabinet = world.get_body_by_name("cabinet")
-        contact_detector = ContactDetectorNode(
-            name="contact_detector", logger=logger, tracked_object=cylinder
+        self.latest_contact_bodies: Dict[Body, Set[Body]] = defaultdict(set)
+        self.latest_support: Dict[Body, Body] = {}
+        self.context = SegmindContext(
+            world=world,
+            latest_support=self.latest_support,
+            latest_contact_bodies=self.latest_contact_bodies,
         )
+        kin_sim = Executor(self.context)
+
+        contact_detector = ContactDetectorNode(
+            name="contact_detector",
+            logger=logger,
+            tracked_object=cylinder,
+        )
+        contact_detector_v2 = ContactDetectorv2(
+            name="contact_detector_v2",
+            context=self.context,
+        )
+
         loss_of_contact_detector = LossOfContactDetectorNode(
-            name="loss_of_contact_detector", logger=logger, tracked_object=cylinder)
+            name="loss_of_contact_detector", logger=logger, tracked_object=cylinder
+        )
 
         support_detector = SupportDetectorNode(
             name="support_detector", logger=logger, tracked_object=cylinder
         )
-        sc.add_nodes([support_detector,contact_detector,loss_of_contact_detector])
-        support_detector.start_condition = trinary_logic_or(loss_of_contact_detector.observation_variable, contact_detector.observation_variable)
+        sc.add_nodes([support_detector, contact_detector, loss_of_contact_detector])
+        support_detector.start_condition = trinary_logic_or(
+            loss_of_contact_detector.observation_variable,
+            contact_detector.observation_variable,
+        )
 
-        kin_sim = Executor(MotionStatechartContext(world=world))
         kin_sim.compile(motion_statechart=sc)
+        contact_detector_v2.print_something()
+
         assert len([i for i in logger.get_events() if isinstance(i, ContactEvent)]) == 0
-        assert len([i for i in logger.get_events() if isinstance(i, LossOfContactEvent)]) == 0
+        assert (
+            len([i for i in logger.get_events() if isinstance(i, LossOfContactEvent)])
+            == 0
+        )
         assert len([i for i in logger.get_events() if isinstance(i, SupportEvent)]) == 0
-        assert len([i for i in logger.get_events() if isinstance(i, LossOfSupportEvent)]) == 0
+        assert (
+            len([i for i in logger.get_events() if isinstance(i, LossOfSupportEvent)])
+            == 0
+        )
         assert support_detector.observation_state == 0.0 or 0.5
         assert loss_of_contact_detector.observation_state == 0.0 or 0.5
         assert contact_detector.observation_state == 0.0 or 0.5
         kin_sim.tick()
 
-        cylinder.parent_connection.origin = HomogeneousTransformationMatrix.from_xyz_rpy(
-            x=table.global_pose.x, y=table.global_pose.y, z=table.global_pose.z + 0.2
+        cylinder.parent_connection.origin = (
+            HomogeneousTransformationMatrix.from_xyz_rpy(
+                x=table.global_pose.x,
+                y=table.global_pose.y,
+                z=table.global_pose.z + 0.2,
+            )
         )
         kin_sim.tick()
         kin_sim.tick()
@@ -114,13 +169,23 @@ class TestMotionStatechart:
         assert len([i for i in logger.get_events() if isinstance(i, ContactEvent)]) == 1
         assert len([i for i in logger.get_events() if isinstance(i, SupportEvent)]) == 1
 
-        cylinder.parent_connection.origin = HomogeneousTransformationMatrix.from_xyz_rpy(
-            x=cabinet.global_pose.x, y=cabinet.global_pose.y, z=cabinet.global_pose.z
+        cylinder.parent_connection.origin = (
+            HomogeneousTransformationMatrix.from_xyz_rpy(
+                x=cabinet.global_pose.x,
+                y=cabinet.global_pose.y,
+                z=cabinet.global_pose.z,
+            )
         )
         kin_sim.tick()
         assert support_detector.observation_state == 1
-        assert len([i for i in logger.get_events() if isinstance(i, LossOfContactEvent)]) == 1
-        assert len([i for i in logger.get_events() if isinstance(i, LossOfSupportEvent)]) == 1
+        assert (
+            len([i for i in logger.get_events() if isinstance(i, LossOfContactEvent)])
+            == 1
+        )
+        assert (
+            len([i for i in logger.get_events() if isinstance(i, LossOfSupportEvent)])
+            == 1
+        )
 
     def visualize(self, world):
         rclpy.init()
