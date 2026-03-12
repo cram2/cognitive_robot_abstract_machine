32725e893bbfdb93706da4fbc137b401d9765cd9
Simon Stelter <stelter@uni-bremen.de>
2026-03-12 13:31:25 +0100

    test huge robot

--- test/giskardpy_test/conftest.py
+++ test/giskardpy_test/conftest.py
@@ -1,19 +1,11 @@
-from copy import deepcopy
-
 import pytest
 
 from semantic_digital_twin.datastructures.prefixed_name import PrefixedName
-from semantic_digital_twin.spatial_types import Vector3, HomogeneousTransformationMatrix
-from semantic_digital_twin.spatial_types.derivatives import DerivativeMap
+from semantic_digital_twin.spatial_types import Vector3
 from semantic_digital_twin.world import World
 from semantic_digital_twin.world_description.connections import (
     RevoluteConnection,
 )
-from semantic_digital_twin.world_description.degree_of_freedom import (
-    DegreeOfFreedomLimits,
-)
-from semantic_digital_twin.world_description.geometry import Cylinder, Sphere
-from semantic_digital_twin.world_description.shape_collection import ShapeCollection
 from semantic_digital_twin.world_description.world_entity import (
     Body,
 )
@@ -29,134 +21,4 @@ def mini_world():
             world=world, parent=body, child=body2, axis=Vector3.Z()
         )
         world.add_connection(connection)
-    return world
-
-
-def robot_factory(fucking_huge_link_length: float, vel_limit: float) -> World:
-    fucking_huge_cylinder = ShapeCollection(
-        shapes=[
-            Cylinder(
-                width=fucking_huge_link_length / 10,
-                height=fucking_huge_link_length,
-                origin=HomogeneousTransformationMatrix.from_xyz_rpy(
-                    z=-(fucking_huge_link_length / 2)
-                ),
-            )
-        ]
-    )
-    fucking_huge_sphere = ShapeCollection(
-        shapes=[
-            Sphere(
-                radius=fucking_huge_link_length / 18,
-            )
-        ]
-    )
-    dof_limits = DegreeOfFreedomLimits(
-        lower=DerivativeMap(None, -vel_limit, None, None),
-        upper=DerivativeMap(None, vel_limit, None, None),
-    )
-    world = World()
-    with world.modify_world():
-        # %% joint1
-        root = Body(name=PrefixedName("map"))
-        link1 = Body(
-            name=PrefixedName("link1"),
-            collision=deepcopy(fucking_huge_sphere),
-            visual=deepcopy(fucking_huge_sphere),
-        )
-        world.add_connection(
-            RevoluteConnection.create_with_dofs(
-                world=world,
-                parent=root,
-                child=link1,
-                axis=Vector3.Z(),
-                dof_limits=dof_limits,
-            )
-        )
-
-        # %% joint2
-        link2 = Body(
-            name=PrefixedName("link2"),
-            collision=deepcopy(fucking_huge_cylinder),
-            visual=deepcopy(fucking_huge_cylinder),
-        )
-        world.add_connection(
-            RevoluteConnection.create_with_dofs(
-                world=world,
-                parent=link1,
-                child=link2,
-                axis=Vector3.X(),
-                dof_limits=dof_limits,
-                connection_T_child_expression=HomogeneousTransformationMatrix.from_xyz_rpy(
-                    z=fucking_huge_link_length,
-                ),
-            )
-        )
-
-        # %% joint3
-        link3 = Body(
-            name=PrefixedName("link3"),
-            collision=deepcopy(fucking_huge_cylinder),
-            visual=deepcopy(fucking_huge_cylinder),
-        )
-        world.add_connection(
-            RevoluteConnection.create_with_dofs(
-                world=world,
-                parent=link2,
-                child=link3,
-                axis=Vector3.X(),
-                dof_limits=dof_limits,
-                connection_T_child_expression=HomogeneousTransformationMatrix.from_xyz_rpy(
-                    z=fucking_huge_link_length,
-                ),
-            )
-        )
-
-        # %% joint4
-        link4 = Body(
-            name=PrefixedName("link4"),
-            collision=deepcopy(fucking_huge_sphere),
-            visual=deepcopy(fucking_huge_sphere),
-        )
-        world.add_connection(
-            RevoluteConnection.create_with_dofs(
-                world=world,
-                parent=link3,
-                child=link4,
-                dof_limits=dof_limits,
-                axis=Vector3.Z(),
-            )
-        )
-
-        # %% joint5
-        link5 = Body(
-            name=PrefixedName("link5"),
-            collision=deepcopy(fucking_huge_sphere),
-            visual=deepcopy(fucking_huge_sphere),
-        )
-        world.add_connection(
-            RevoluteConnection.create_with_dofs(
-                world=world,
-                parent=link4,
-                child=link5,
-                axis=Vector3.X(),
-                dof_limits=dof_limits,
-            )
-        )
-
-        # %% joint6
-        eef = Body(
-            name=PrefixedName("eef"),
-            collision=deepcopy(fucking_huge_sphere),
-            visual=deepcopy(fucking_huge_sphere),
-        )
-        world.add_connection(
-            RevoluteConnection.create_with_dofs(
-                world=world,
-                parent=link5,
-                child=eef,
-                axis=Vector3.Y(),
-                dof_limits=dof_limits,
-            )
-        )
     return world
--- test/giskardpy_test/test_qp/test_fucking_huge_robot.py
+++ test/giskardpy_test/test_qp/test_fucking_huge_robot.py
@@ -1,4 +1,5 @@
-from conftest import robot_factory
+from copy import deepcopy
+
 from giskardpy.executor import Executor, SimulationPacer
 from giskardpy.motion_statechart.context import MotionStatechartContext
 from giskardpy.motion_statechart.goals.templates import Sequence
@@ -10,13 +11,157 @@ from giskardpy.motion_statechart.motion_statechart import MotionStatechart
 from giskardpy.motion_statechart.tasks.cartesian_tasks import (
     CartesianPosition,
 )
-from giskardpy.motion_statechart.tasks.joint_tasks import JointPositionList
 from giskardpy.qp.qp_controller_config import QPControllerConfig
 from semantic_digital_twin.adapters.ros.visualization.viz_marker import (
     VizMarkerPublisher,
 )
 from semantic_digital_twin.datastructures.joint_state import JointState
+from semantic_digital_twin.datastructures.prefixed_name import PrefixedName
 from semantic_digital_twin.spatial_types import Point3
+from semantic_digital_twin.spatial_types import Vector3, HomogeneousTransformationMatrix
+from semantic_digital_twin.spatial_types.derivatives import DerivativeMap
+from semantic_digital_twin.world import World
+from semantic_digital_twin.world_description.connections import (
+    RevoluteConnection,
+)
+from semantic_digital_twin.world_description.degree_of_freedom import (
+    DegreeOfFreedomLimits,
+)
+from semantic_digital_twin.world_description.geometry import Cylinder, Sphere
+from semantic_digital_twin.world_description.shape_collection import ShapeCollection
+from semantic_digital_twin.world_description.world_entity import (
+    Body,
+)
+
+
+def robot_factory(fucking_huge_link_length: float, vel_limit: float) -> World:
+    fucking_huge_cylinder = ShapeCollection(
+        shapes=[
+            Cylinder(
+                width=fucking_huge_link_length / 10,
+                height=fucking_huge_link_length,
+                origin=HomogeneousTransformationMatrix.from_xyz_rpy(
+                    z=-(fucking_huge_link_length / 2)
+                ),
+            )
+        ]
+    )
+    fucking_huge_sphere = ShapeCollection(
+        shapes=[
+            Sphere(
+                radius=fucking_huge_link_length / 18,
+            )
+        ]
+    )
+    dof_limits = DegreeOfFreedomLimits(
+        lower=DerivativeMap(None, -vel_limit, None, None),
+        upper=DerivativeMap(None, vel_limit, None, None),
+    )
+    world = World()
+    with world.modify_world():
+        # %% joint1
+        root = Body(name=PrefixedName("map"))
+        link1 = Body(
+            name=PrefixedName("link1"),
+            collision=deepcopy(fucking_huge_sphere),
+            visual=deepcopy(fucking_huge_sphere),
+        )
+        world.add_connection(
+            RevoluteConnection.create_with_dofs(
+                world=world,
+                parent=root,
+                child=link1,
+                axis=Vector3.Z(),
+                dof_limits=dof_limits,
+            )
+        )
+
+        # %% joint2
+        link2 = Body(
+            name=PrefixedName("link2"),
+            collision=deepcopy(fucking_huge_cylinder),
+            visual=deepcopy(fucking_huge_cylinder),
+        )
+        world.add_connection(
+            RevoluteConnection.create_with_dofs(
+                world=world,
+                parent=link1,
+                child=link2,
+                axis=Vector3.X(),
+                dof_limits=dof_limits,
+                connection_T_child_expression=HomogeneousTransformationMatrix.from_xyz_rpy(
+                    z=fucking_huge_link_length,
+                ),
+            )
+        )
+
+        # %% joint3
+        link3 = Body(
+            name=PrefixedName("link3"),
+            collision=deepcopy(fucking_huge_cylinder),
+            visual=deepcopy(fucking_huge_cylinder),
+        )
+        world.add_connection(
+            RevoluteConnection.create_with_dofs(
+                world=world,
+                parent=link2,
+                child=link3,
+                axis=Vector3.X(),
+                dof_limits=dof_limits,
+                connection_T_child_expression=HomogeneousTransformationMatrix.from_xyz_rpy(
+                    z=fucking_huge_link_length,
+                ),
+            )
+        )
+
+        # %% joint4
+        link4 = Body(
+            name=PrefixedName("link4"),
+            collision=deepcopy(fucking_huge_sphere),
+            visual=deepcopy(fucking_huge_sphere),
+        )
+        world.add_connection(
+            RevoluteConnection.create_with_dofs(
+                world=world,
+                parent=link3,
+                child=link4,
+                dof_limits=dof_limits,
+                axis=Vector3.Z(),
+            )
+        )
+
+        # %% joint5
+        link5 = Body(
+            name=PrefixedName("link5"),
+            collision=deepcopy(fucking_huge_sphere),
+            visual=deepcopy(fucking_huge_sphere),
+        )
+        world.add_connection(
+            RevoluteConnection.create_with_dofs(
+                world=world,
+                parent=link4,
+                child=link5,
+                axis=Vector3.X(),
+                dof_limits=dof_limits,
+            )
+        )
+
+        # %% joint6
+        eef = Body(
+            name=PrefixedName("eef"),
+            collision=deepcopy(fucking_huge_sphere),
+            visual=deepcopy(fucking_huge_sphere),
+        )
+        world.add_connection(
+            RevoluteConnection.create_with_dofs(
+                world=world,
+                parent=link5,
+                child=eef,
+                axis=Vector3.Y(),
+                dof_limits=dof_limits,
+            )
+        )
+    return world
 
 
 def execute(link_length: float, vel_limit: float, rclpy_node):
