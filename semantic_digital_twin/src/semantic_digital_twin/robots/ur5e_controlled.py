674f537f6aea6b6ff907bb632b4a78fa2f883b62
LucaKro <luc_kro@uni-bremen.de>
2026-04-23 14:46:52 +0200

    ported ur5e to new structure

--- semantic_digital_twin/src/semantic_digital_twin/robots/ur5e_controlled.py
+++ semantic_digital_twin/src/semantic_digital_twin/robots/ur5e_controlled.py
@@ -1,32 +1,56 @@
+from __future__ import annotations
+
 from abc import ABC
 from dataclasses import dataclass
+from typing import Self
 
-from semantic_digital_twin.robots.robot_parts import (
-    Arm,
-    Finger,
-    ParallelGripper,
-    AbstractRobot,
+from semantic_digital_twin.datastructures.definitions import (
+    GripperState,
+    StaticJointState,
 )
+from semantic_digital_twin.datastructures.joint_state import JointState
+from semantic_digital_twin.datastructures.prefixed_name import PrefixedName
 from semantic_digital_twin.robots.robot_part_mixins import (
     HasOneArm,
     HasParallelGripper,
 )
-from semantic_digital_twin.spatial_types import Quaternion
-from semantic_digital_twin.world import World
+from semantic_digital_twin.robots.robot_parts import (
+    AbstractRobot,
+    Arm,
+    Finger,
+    ParallelGripper,
+)
+from semantic_digital_twin.spatial_types import Quaternion, Vector3
+from semantic_digital_twin.world_description.world_entity import (
+    KinematicStructureEntity,
+)
 
 
 @dataclass(eq=False)
-class UR5ControlledFinger(Finger, ABC): ...
+class UR5ControlledFinger(Finger, ABC):
+
+    def setup_hardware_interfaces(self):
+        pass
+
+    def setup_joint_states(self):
+        pass
 
 
 @dataclass(eq=False)
 class UR5ControlledLeftFinger(UR5ControlledFinger):
 
     @classmethod
-    def setup_default_configuration_in_world(cls, world: World):
+    def setup_default_configuration_in_world_below_robot_root(
+        cls, robot_root: KinematicStructureEntity
+    ) -> Self:
+        world = robot_root._world
         finger = cls(
-            root=world.get_body_by_name("robotiq_85_left_finger_link"),
-            tip=world.get_body_by_name("robotiq_85_left_finger_tip_link"),
+            root=world.get_body_in_branch_by_name(
+                robot_root, "robotiq_85_left_finger_link"
+            ),
+            tip=world.get_body_in_branch_by_name(
+                robot_root, "robotiq_85_left_finger_tip_link"
+            ),
         )
         world.add_semantic_annotation(finger)
         return finger
@@ -36,10 +60,17 @@ class UR5ControlledLeftFinger(UR5ControlledFinger):
 class UR5ControlledRightFinger(UR5ControlledFinger):
 
     @classmethod
-    def setup_default_configuration_in_world(cls, world: World):
+    def setup_default_configuration_in_world_below_robot_root(
+        cls, robot_root: KinematicStructureEntity
+    ) -> Self:
+        world = robot_root._world
         finger = cls(
-            root=world.get_body_by_name("robotiq_85_right_finger_link"),
-            tip=world.get_body_by_name("robotiq_85_right_finger_tip_link"),
+            root=world.get_body_in_branch_by_name(
+                robot_root, "robotiq_85_right_finger_link"
+            ),
+            tip=world.get_body_in_branch_by_name(
+                robot_root, "robotiq_85_right_finger_tip_link"
+            ),
         )
         world.add_semantic_annotation(finger)
         return finger
@@ -48,23 +79,63 @@ class UR5ControlledRightFinger(UR5ControlledFinger):
 @dataclass(eq=False)
 class UR5ControlledGripper(ParallelGripper):
 
+    def setup_hardware_interfaces(self):
+        self._setup_hardware_interfaces_for_active_connections()
+
+    def setup_joint_states(self):
+        gripper_joints = self.active_connections
+
+        gripper_open = JointState.from_mapping(
+            name=PrefixedName("gripper_open", prefix=self.name.name),
+            mapping=dict(
+                zip(
+                    gripper_joints,
+                    [
+                        0.798,
+                        0.00366,
+                        0.796,
+                        -0.793,
+                        0.798,
+                        0.00366,
+                        0.796,
+                        -0.793,
+                    ],
+                )
+            ),
+            state_type=GripperState.OPEN,
+        )
+
+        gripper_close = JointState.from_mapping(
+            name=PrefixedName("gripper_close", prefix=self.name.name),
+            mapping=dict(zip(gripper_joints, [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])),
+            state_type=GripperState.CLOSE,
+        )
+
+        self.add_joint_state(gripper_open)
+        self.add_joint_state(gripper_close)
+
     @classmethod
-    def setup_default_configuration_in_world(cls, world: World):
+    def setup_default_configuration_in_world_below_robot_root(
+        cls, robot_root: KinematicStructureEntity
+    ) -> Self:
+        world = robot_root._world
         gripper = cls(
-            root=world.get_body_by_name("robotiq_gripper-2F-85_link"),
-            tool_frame=world.get_body_by_name("right_pad"),
+            root=world.get_body_in_branch_by_name(
+                robot_root, "robotiq_gripper-2F-85_link"
+            ),
+            tool_frame=world.get_body_in_branch_by_name(robot_root, "right_pad"),
             front_facing_orientation=Quaternion(0, 0, 0, 1),
         )
         world.add_semantic_annotation(gripper)
         return gripper
 
     def setup_finger_semantic_annotations(self):
-        thumb = UR5ControlledLeftFinger.setup_default_configuration_in_world(
-            self._world
+        thumb = UR5ControlledLeftFinger.setup_default_configuration_in_world_below_robot_root(
+            self.root
         )
         self.add_thumb(thumb)
-        finger = UR5ControlledRightFinger.setup_default_configuration_in_world(
-            self._world
+        finger = UR5ControlledRightFinger.setup_default_configuration_in_world_below_robot_root(
+            self.root
         )
         self.add_finger(finger)
 
@@ -72,33 +143,56 @@ class UR5ControlledGripper(ParallelGripper):
 @dataclass(eq=False)
 class UR5ControlledArm(Arm, HasParallelGripper):
 
+    def setup_hardware_interfaces(self):
+        self._setup_hardware_interfaces_for_active_connections()
+
+    def setup_joint_states(self):
+        arm_park = JointState.from_mapping(
+            name=PrefixedName("arm_park", prefix=self.name.name),
+            mapping=dict(
+                zip(
+                    self.active_connections,
+                    [3.14, -1.56, 1.58, -1.57, -1.57, 0.0],
+                )
+            ),
+            state_type=StaticJointState.PARK,
+        )
+
+        self.add_joint_state(arm_park)
+
     @classmethod
-    def setup_default_configuration_in_world(cls, world: World):
+    def setup_default_configuration_in_world_below_robot_root(
+        cls, robot_root: KinematicStructureEntity
+    ) -> Self:
+        world = robot_root._world
         arm = cls(
-            root=world.get_body_by_name("base_link"),
-            tip=world.get_body_by_name("wrist_3_link"),
+            root=world.get_body_in_branch_by_name(robot_root, "base_link"),
+            tip=world.get_body_in_branch_by_name(robot_root, "wrist_3_link"),
         )
         world.add_semantic_annotation(arm)
         return arm
 
     def setup_end_effector_semantic_annotation(self):
-        gripper = UR5ControlledGripper.setup_default_configuration_in_world(self._world)
+        gripper = (
+            UR5ControlledGripper.setup_default_configuration_in_world_below_robot_root(
+                self.root
+            )
+        )
         self.add_end_effector(gripper)
         gripper.setup_finger_semantic_annotations()
 
 
 @dataclass(eq=False)
 class UR5Controlled(AbstractRobot, HasOneArm):
-    """
-    Class that describes the UR5 controlled Robot.
-    """
 
     @classmethod
     def _get_root_body_name(cls) -> str:
         return "base_link"
 
     def setup_arm_semantic_annotations(self):
-        arm = UR5ControlledArm.setup_default_configuration_in_world(self._world)
+        arm = UR5ControlledArm.setup_default_configuration_in_world_below_robot_root(
+            self.root
+        )
         self.add_arm(arm)
         arm.setup_end_effector_semantic_annotation()
 
