901138a8a6ab8251ea917453fb3ed9e56ccac774
fkenghagho <fkenghag@uni-bremen.de>
2026-06-17 11:38:49 +0200

    remove giskard world root

--- giskardpy/src/giskardpy/model/world_config.py
+++ giskardpy/src/giskardpy/model/world_config.py
@@ -59,7 +59,6 @@ class WorldWithFixedRobot(WorldConfig):
 
     def setup_world(self):
         map = Body(name=self.root_name)
-        self.world.add_connection(map)
 
         urdf_parser = URDFParser(urdf=self.urdf, prefix="")
         world_with_robot = urdf_parser.parse()
@@ -67,7 +66,7 @@ class WorldWithFixedRobot(WorldConfig):
         self.robot_root = world_with_robot.root
         map_C_robot = FixedConnection(parent=map, child=self.robot_root)
 
-        self.world.merge_world(world_with_robot, map_C_robot)
+        self.world.merge_world(world_with_robot)
 
 
 @dataclass
