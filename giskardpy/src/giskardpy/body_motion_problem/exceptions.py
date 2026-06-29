10a8406d2f987643cac61515eb627f628f7dada6
maltehue <huerkamp@uni-bremen.de>
2026-06-29 15:16:42 +0200

    removed unused file

--- giskardpy/src/giskardpy/body_motion_problem/exceptions.py
+++
@@ -1,9 +0,0 @@
-"""
-Exceptions for giskardpy BMP physics model implementations.
-"""
-
-
-class MissingMotionStatechartError(Exception):
-    """
-    Raised when a physics model is asked to run without a configured MotionStatechart.
-    """
